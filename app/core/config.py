2c950c452c17c6aa65dcd6fe8c69fd56e8ddb93a
Sihuan Newrise <sihuannewrise@gmail.com>
2023-02-24 23:56:10 +0300

    alembic added

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,9 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Заявки на оплату'
     app_description: str = 'Передача заявок в оплату'
-    
+
+    database_url: str
+
     ydb_endpoint: str
     ydb_database: str
 
---
+++ app/core/models/counteragent.py.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, UniqueConstraint
+
+from app.core.db import Base
+
+
+class CounterAgent(Base):
+    name = Column(String(100), nullable=False)
+    full_name = Column(String(150))
+
+    inn = Column(Integer, nullable=False)
+    kpp = Column(Integer, nullable=False)
+    __table_args__ = (UniqueConstraint('inn', 'kpp', name='_inn_kpp_unique'),)
