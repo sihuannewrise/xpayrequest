890f2b013b2dcf318f93a9b340a62ac002b650c1
Sihuan Newrise <sihuannewrise@gmail.com>
2023-02-25 00:18:18 +0300

    db conf

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,16 @@
 import asyncio
+
 import os
 from dotenv import load_dotenv
+
 from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
+
+from app.core.base import Base
 
 load_dotenv('ENV/.env')
 # this is the Alembic Config object, which provides
@@ -23,7 +27,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
---
+++ app/core/base.py
@@ -0,0 +1,3 @@
+"""Импорты класса Base и всех моделей для Alembic."""
+from app.core.db import Base # noqa
+from app.core.models.counteragent import CounterAgent # noqa
