392686e0e03c22dc0ab973cf13f52f5df01891fb
sihuannewrise <sihuannewrise@gmail.com>
2023-02-27 17:05:04 +0300

    banks

--- app/core/models/banks.py
+++ app/core/models/banks.py
@@ -4,6 +4,7 @@ from app.core.db import Base
 
 
 class Banks(Base):
-    bic = Column(Integer(), nullable=False)
+    bic = Column(Integer(), unique=True, nullable=False)
     name = Column(String(100), nullable=False)
     city = Column(String(150), nullable=False)
+    correspondent_account = 
