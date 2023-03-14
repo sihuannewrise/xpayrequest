3834eade8f253a007ebbe64f537aa38dad31de9f
sihuannewrise <sihuannewrise@gmail.com>
2023-03-14 15:37:18 +0300

    proc

--- app/core/models/paymentprocessing.py
+++ app/core/models/paymentprocessing.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from uuid import UUID
 
-from sqlalchemy import ForeignKey, UniqueConstraint
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from app.core.db import Base
@@ -12,7 +12,3 @@ class PaymentProcessing(Base):
     user_id: Mapped[UUID] = mapped_column(ForeignKey('user.id'))
     verdict: Mapped[str]
     processed_at: Mapped[datetime]
-
-    __table_args__ = (UniqueConstraint(
-        'pr_id', 'user_id', 'verdict', name='_pr_user_verdict_unique',
-    ),)
