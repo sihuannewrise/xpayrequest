8c93271f95572651f81158a4304d00ea2a281893
Sihuan Newrise <sihuannewrise@gmail.com>
2023-03-26 18:23:38 +0300

    templates added

--- app/core/dependencies/base.py
+++ app/core/dependencies/base.py
@@ -1,11 +1,12 @@
-from collections.abc import Mapping
+from typing import Mapping
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.crud.base import CRUDBase
 from app.core.exceptions.base import ObjectNotFoundError
 
 
-async def valid_obj_id(model, obj_id: int) -> Mapping:
-    obj = await CRUDBase.get(obj_id)
+async def valid_obj_id(model, obj_id: int, session: AsyncSession) -> Mapping:
+    obj = await CRUDBase.get(model, obj_id, session)
     if not obj:
         raise ObjectNotFoundError()
     return obj
