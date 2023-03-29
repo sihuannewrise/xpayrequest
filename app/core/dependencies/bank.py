5dd2b56010a72e31088c3efa8031beb75e1bb5d7
sihuannewrise <sihuannewrise@gmail.com>
2023-03-29 16:54:44 +0300

    dependency bank

--- app/core/dependencies/bank.py
+++ app/core/dependencies/bank.py
@@ -1,9 +1,7 @@
 from typing import Mapping
 
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.core.crud.base import CRUDBase as crudbase
 from app.core.dependencies.base import check_obj_not_exist
 from app.core.exceptions.base import (
     ObjectAlreadyExistsError,
@@ -15,7 +13,7 @@ async def check_bank_not_exist(
     bic_val: str,
     session: AsyncSession,
 ) -> Mapping:
-    bank = await check_obj_not_exist(Bank, 'bic', bic_val)
+    bank = await check_obj_not_exist(Bank, 'bic', bic_val, session)
     if bank is not None:
         raise ObjectAlreadyExistsError()
     return bank
