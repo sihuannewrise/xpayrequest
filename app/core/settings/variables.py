cee1fc7f0b6da029d833bb9604791dcc8658a4e2
sihuannewrise <sihuannewrise@gmail.com>
2023-03-31 15:46:56 +0300

    forms

--- app/core/settings/variables.py
+++ app/core/settings/variables.py
@@ -1,8 +1,13 @@
+# counteragents and banks
 INN_LEN = (10, 12)
 KPP_LEN = 9
 BIC_LEN = 9
 SWIFT_LEN = 11
 CORR_ACC_LEN = 20
 
+# pagination
 PAG_LIMIT = 10
 PAG_OFFSET = 0
+
+# user fields
+MIN_PWD_LEN = 6
