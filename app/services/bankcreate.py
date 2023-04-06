182b51e70d9ca9ed7e03a3db23351c53cfee5ae2
sihuannewrise <sihuannewrise@gmail.com>
2023-04-06 17:12:56 +0300

    add_all cleared data

--- app/services/bankcreate.py
+++ app/services/bankcreate.py
@@ -6,7 +6,8 @@ import aiofiles
 from datetime import datetime as dt
 from contextlib import asynccontextmanager
 
-from app.services.dadata import dadata_find_bank
+from sqlalchemy import select
+from app.services.dadata import dd_find_bank
 from app.core.db import get_async_session
 from app.core.models.bank import Bank
 
@@ -46,7 +47,7 @@ async def check_no_bic_duplicate(bic: str, session):
         )
 
 
-async def get_bank_by_id(id: str):
+async def dd_get_bank_by_id(id: str):
     """
     Получает из сервиса dadata информацию о банке по его ID (БИК, ИНН, свифт).
     Args:
@@ -58,7 +59,7 @@ async def get_bank_by_id(id: str):
     из поля "казначейские счета" (treasury_accounts) берет из списка
     первое значение.
     """
-    data = await dadata_find_bank(id)
+    data = await dd_find_bank(id)
     if not data:
         raise IndexError(f'DaData: нет данных по \033[1m{id}\033[0m')
     data = data[0]
@@ -87,6 +88,43 @@ async def get_bank_by_id(id: str):
         bank['treasury_accounts'], list
     ):
         bank['treasury_accounts'] = bank['treasury_accounts'][0]
+    return bank
+
+
+async def stuff_bank_with_data(
+    bank_info: dict, is_archived: bool = False,
+):
+    data = bank_info[0]
+    bank = {
+        'name': data['value'],
+        'bic': data['data']['bic'],
+        'address': data['data']['address']['value'],
+        'status': data['data']['state']['status'],
+        'inn': data['data']['inn'],
+        'kpp': data['data']['kpp'],
+        'actuality_date': data['data']['state']['actuality_date'],
+        'registration_date': data['data']['state']['registration_date'],
+        'liquidation_date': data['data']['state']['liquidation_date'],
+        'correspondent_account': data['data']['correspondent_account'],
+        'payment_city': data['data']['payment_city'],
+        'swift': data['data']['swift'],
+        'registration_number': data['data']['registration_number'],
+        'treasury_accounts': data['data']['treasury_accounts'],
+        'opf_type': data['data']['opf']['type'],
+    }
+    bank = {k: v for k, v in bank.items() if v is not None}
+    for field in DATE_FIELDS:
+        if field in bank:
+            bank[field] = dt.fromtimestamp(bank[field]/1000)
+    if 'treasury_accounts' in bank and isinstance(
+        bank['treasury_accounts'], list
+    ):
+        bank['treasury_accounts'] = bank['treasury_accounts'][0]
+    extra_fields = {
+        'is_archived': is_archived,
+        'description': 'autoloaded from DAData',
+    }
+    bank.update(extra_fields)
     return bank
 
 
@@ -122,46 +160,45 @@ async def bank_create(
         print(e)
 
 
-async def add_banks(bics: set):
+async def add_banks(bics: set) -> None:
+    """
+    Записывает в БД по одной модели из списка БИКов (метод add).
+    """
     async with get_async_session_context() as session:
         for bic in bics:
             try:
-                dadata = await get_bank_by_id(bic)
+                dadata = await dd_get_bank_by_id(bic)
                 await bank_create(bic, dadata, session)
             except Exception as e:
                 print(e)
 
 
-async def add_all_bank(
-    bic: str, bank_info: dict, session, is_archived: bool = False,
-):
+async def add_all_banks(bics: set) -> None:
     """
-    Создание в БД записи о новом банке.
-    Args:
-        bic (str) - БИК, bank_info (dict) - словарь с данными по банку
-    Returns:
-        Данные банка, записанные в БД.
-
-    Данные должны поступать корректные (не None, например).
-    Перед записью сверяет, нет ли в БД существующей записи с БИКом.
-    Добавляет в словарь дополнительные ключи is_archived и description.
+    Получает из БД список всех БИКов. Отфильтровывает вх данные, отсеивая те,
+    что уже есть в БД. Оставшиеся БИКи обрабатываются дальше.
+    Создает список моделей с данными банков и записывает в БД сразу
+    все банки из списка (метод add_all).
     """
-    try:
-        await check_no_bic_duplicate(bic, session)
-        extra_fields = {
-            'is_archived': is_archived,
-            'description': 'autoloaded from DAData',
-        }
-        bank_info.update(extra_fields)
-        model = Bank()
-        for field in bank_info:
-            setattr(model, field, bank_info[field])
-        session.add(model)
+    async with get_async_session_context() as session:
+        real_banks = []
+        bics_from_db = await session.scalars(select(Bank.bic))
+        bics_from_db = bics_from_db.all()
+        raw_bics = set(bics).difference(set(bics_from_db))
+        for bic in raw_bics:
+            candidate_bank = await dd_find_bank(bic)
+            if not candidate_bank:
+                continue
+            new_bank = await stuff_bank_with_data(
+                candidate_bank, is_archived=False,
+            )
+            model = Bank()
+            for field in new_bank:
+                setattr(model, field, new_bank[field])
+            real_banks.append(model)
+        session.add_all(real_banks)
         await session.commit()
-        await session.refresh(model)
-        return model
-    except Exception as e:
-        print(e)
+        return None
 
 
 if __name__ == "__main__":
@@ -170,4 +207,4 @@ if __name__ == "__main__":
 
     # asyncio.run(get_bic_list('app/services/config/biclist.txt'))
 
-    asyncio.run(add_banks(BICS))
+    asyncio.run(add_all_banks(BICS))
--- app/services/dadata.py
+++ app/services/dadata.py
@@ -5,17 +5,17 @@ from dadata import DadataAsync
 from app.core.config import settings
 
 
-async def dadata_find_bank(id: str):
+async def dd_find_bank(id: str):
     async with DadataAsync(settings.dd_token, settings.dd_secret,) as dadata:
         result = await dadata.find_by_id(name='bank', query=id)
         return result
 
 
-async def dadata_suggest_bank(name: str):
+async def dd_suggest_bank(name: str):
     async with DadataAsync(settings.dd_token, settings.dd_secret,) as dadata:
         result = await dadata.suggest(name='bank', query=name)
         return result
 
 
 if __name__ == "__main__":
-    print(asyncio.run(dadata_find_bank('007182108')))
+    print(asyncio.run(dd_find_bank('007182108')))
