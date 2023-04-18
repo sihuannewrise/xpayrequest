e1bde48d37e72c6e6b76872444c15088374cab91
sihuannewrise <sihuannewrise@gmail.com>
2023-04-18 17:00:20 +0300

    bank create

--- app/services/bankaccountcreate.py
+++ app/services/bankaccountcreate.py
@@ -3,15 +3,15 @@
 import os
 import asyncio
 import aiofiles
-from datetime import datetime as dt
+
 from contextlib import asynccontextmanager
 
 from sqlalchemy import select
-from app.services.dadata import dd_find_by_id
 from app.core.db import get_async_session
-from app.core.models.bankaccount import BankAccount
+from app.core.models import Bank, BankAccount, Currency
 
 from app.services.config.accounts import ACCOUNTS
+from app.services.config.currency_ids import CURRENCY_IDS
 
 get_async_session_context = asynccontextmanager(get_async_session)
 
@@ -32,45 +32,77 @@ async def get_account_list(filename):
                 print(f"'{acc}': {{'{bic}': {list(inn)}}},")
 
 
-async def check_account_duplicate(model, acc: str, session):
-    account = await session.scalar(select(model.account).where(
-        model.account == acc)
-    )
-    if account is not None:
-        raise ValueError(
-            f'Счет \033[1m{acc}\033[0m уже в таблице '
+async def write_currencies_to_file():
+    async with get_async_session_context() as session:
+        currency_ids = await session.execute(
+            select(Currency.name, Currency.id))
+        currency_ids = currency_ids.all()
+    async with aiofiles.open(
+        'app/services/config/currency_ids.py', mode='w'
+    ) as file:
+        my_ids = {}
+        for id in currency_ids:
+            my_ids[id[0]] = id[1]
+        await file.write(f'CURRENCY_IDS = {my_ids}\n')
+
+
+async def check_duplicate_by_field(model, field, field_value, session):
+    col = getattr(model, field)
+    duplicate = await session.scalar(select(col).where(col == field_value))
+    if duplicate is not None:
+        raise IndexError(
+            f'Значение {field}=\033[1m{field_value}\033[0m уже в таблице '
             f'\033[1m{model.__name__.lower()}\033[0m !'
         )
 
 
-async def add_account(bics: set) -> None:
-    """
-    """
-    async with get_async_session_context() as session:
-        real_banks = []
-        bics_from_db = await session.scalars(select(Bank.bic))
-        bics_from_db = bics_from_db.all()
-        raw_bics = set(bics).difference(set(bics_from_db))
-        for bic in raw_bics:
-            candidate_bank = await dd_find_by_id(
-                DD_SEARCH_SUBJECT['bank'], bic,
+async def add_bank_account(model, acc_num: str, bic: str, session):
+    try:
+        await check_duplicate_by_field(model, 'account', acc_num, session)
+    except IndexError as e:
+        print(e)
+    else:
+        try:
+            await check_duplicate_by_field(Bank, 'bic', acc_num, session)
+        except IndexError as e:
+            print(e)
+        else:
+            acc_model = model(
+                account=acc_num,
+                bank_bic=bic,
+                currency_id=CURRENCY_IDS[acc_num[5:8]],
+                description=f'autoloaded by {os.path.basename(__file__)}'
             )
-            if not candidate_bank:
-                continue
-            new_bank = await stuff_bank_with_data(
-                candidate_bank[0], is_archived=False,
-                description=f'autoloaded from {os.path.basename(__file__)}'
-            )
-            model = Bank()
-            for field in new_bank:
-                setattr(model, field, new_bank[field])
-            real_banks.append(model)
-        if real_banks:
-            session.add_all(real_banks)
+            session.add(acc_model)
             await session.commit()
+
+
+async def add_multi_records(data: dict) -> None:
+    async with get_async_session_context() as session:
+        # await write_currencies_to_file()
+        for account, vals in data.items():
+            for bic, inn_list in vals.items():
+                try:
+                    await add_bank_account(
+                        BankAccount, account, bic, session)
+                except ValueError as e:
+                    print(e)
+            # else:
+            #     for kpp in kpps:
+            #         await add_to_kpp(KPP, kpp, session)
+
+            #         try:
+            #             await add_to_cakppmapping(
+            #                 CaKppMapping, 'ca_inn', inn,
+            #                 'kpp_name', kpp, session,
+            #             )
+            #         except ValueError as e:
+            #             print(e)
         return None
 
 
 if __name__ == "__main__":
     # asyncio.run(get_account_list('app/services/config/accounts.txt'))
-    asyncio.run()
+    # asyncio.run(write_ids_to_file())
+
+    asyncio.run(add_multi_records(ACCOUNTS))
